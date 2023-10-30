d29a391ee76ecb4fb83d4783dec0acbb13343458
Alpha <alpha.likembe@student.moringaschool.com>
2023-10-30 21:44:42 +0300

    Complete

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 15, "M", "White", "Angry", "True");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "F", "Brown", "Polite", "True");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 12, "M", "Black", "Calm", "False");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 18, "F", "Brown", "Smily", "True");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 20, "M", "Black", "Calm", "False");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 21, "M", "Green", "Angry", "True");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 17, "F", "White", "Polite", "False");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 15, "M", "Black", "Hostile", "True");
+
+
+
+
+
